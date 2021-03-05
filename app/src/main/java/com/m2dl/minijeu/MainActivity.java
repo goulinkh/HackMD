bdccb836a11445b8a0b3874622acc28ddc7a614c
ludovicB31 <ludovicblan@yahoo.fr>
2021-03-05 17:15:05 +0100

    Ajout des conteneurs Game et Buttons

--- app/src/main/java/com/m2dl/minijeu/MainActivity.java
+++ app/src/main/java/com/m2dl/minijeu/MainActivity.java
@@ -2,16 +2,24 @@ package com.m2dl.minijeu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+    private LinearLayout buttonsLayout;
+    private LinearLayout gameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
         int valeur_y = sharedPref.getInt("valeur_y", 0);
@@ -29,6 +37,15 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        setContentView(R.layout.activity_main);
+        start();
+    }
+
+    //Ce code est séparé car il est impossible de manipuler le contunu de la View dans le onCreate
+    private void start(){
+        buttonsLayout=findViewById(R.id.linearLayout_buttons);
+        gameLayout=findViewById(R.id.linearLayout_game);
+        GameView myGameView=new GameView(this);
+        gameLayout.addView(myGameView);
     }
 }
