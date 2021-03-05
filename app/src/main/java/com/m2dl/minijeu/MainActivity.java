111f3ebbc8c0e5eca0146c079cc5c32f9fb81473
Goulin Khoge <goulin.khoge@gmail.com>
2021-03-05 17:29:39 +0100

    Position Sensor listener

--- app/src/main/java/com/m2dl/minijeu/MainActivity.java
+++ app/src/main/java/com/m2dl/minijeu/MainActivity.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorEventListener2;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private GameView gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
@@ -29,6 +38,28 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(new GameView(this));
+        gameView = new GameView(this);
+        setContentView(gameView);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+        Point point = gameView.getCirclePosition();
+        System.out.println("X: " + x + " Y: " + y + " z:" + z);
+
+        point.x += x * 100;
+        point.y += y * 100;
+        gameView.setCirclePosition(point);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
 }
